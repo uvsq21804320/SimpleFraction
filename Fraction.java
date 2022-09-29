07c648f6f5b6c831e3ae28c51088917d0a1b8495
Utilisateur <user@debian.arcahe.ovh>
2022-09-29 16:02:55 +0200

    methodes get, constantes

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,34 @@
 class Fraction {
+    public int _numerateur;
+    public int _denominateur;
+
+    public static final Fraction UN = new Fraction(1);
+    public static final Fraction ZERO = new Fraction();
+
+    public Fraction(int numerateur, int denominateur) {
+        _numerateur = numerateur;
+        _denominateur = denominateur;
+    }
+
+    public Fraction(int numerateur) {
+        _numerateur = numerateur;
+        _denominateur = 1;
+    }
+
+    public Fraction() {
+        _numerateur = 0;
+        _denominateur = 1;
+    }
+
+    public int getDenominateur() {
+        return _denominateur;
+    }
+
+    public int getNumerateur() {
+        return _numerateur;
+    }
+
     public String toString() {
-        return "Je suis une Fraction";
+        return  _numerateur + " / " + _denominateur;
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction frac = new Fraction(); 
-        System.out.println(frac.toString());
+        Fraction frac = new Fraction(11, 18);
+        assert frac.toString().equals("11 / 18") : "Erreur toString";
+        assert frac.getNumerateur() == 11 : "Erreur getNumerateur";
+        assert frac.getDenominateur() == 18 : "Erreur getDenominateur";
+
     }
 }
